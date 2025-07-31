687138e77af7d5847d4a567e3689be814db13796
Johan Laubscher <johanl@ajenti.co.za>
2025-07-31 08:47:42 +0200

    Current server version with virtual environment.

--- bin/runDelta.sh
+++ bin/runDelta.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python3 -m venv venv_matogen
-source venv_matogen/bin/activate
+
+
+./venv_matogen/bin/activate
 
 set +x 
 set -e  # Exit immediately if a command exits with a non-zero status
@@ -37,6 +38,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 log_message "Reference Tables collected at ${SECONDS} Seconds"
+
 
 #Collect Account related data
 log_message "Collect Account related data"
