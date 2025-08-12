0c0d5c4d542740e96984f919f31a8d342612bba4
Johan Laubscher <johanl@ajenti.co.za>
2025-08-12 10:44:25 +0200

    Changed the scripts to change directory to the script directory when starting.

--- bin/runHistory.sh
+++ bin/runHistory.sh
@@ -3,6 +3,8 @@
 # Get full path to script location
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+cd $SCRIPT_DIR
+
 # Activate the virtual environment (use full path)
 #source "${SCRIPT_DIR}/venv_matogen/bin/activate"
 source "${HOME}/venv_matogen/bin/activate"
